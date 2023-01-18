910c804b09f1bb4921312e93fa31d161cfb27f54
amanmdesai <amanmukeshdesai@gmail.com>
2023-01-18 13:21:07 +0530

    add

--- tests/test_py.py
+++ tests/test_py.py
@@ -5,7 +5,10 @@ import os
 def test_eventGen():
     pymcabc.DefineProcess('A A > B B',mA=4,mB=10,mC=1,Ecm=30)
     pymcabc.CrossSection().calc_xsection()
+    pymcabc.SaveEvent(10000,boolDecay=True,boolDetector=True).to_root('name.root')
     pymcabc.PlotData.file('name.root')
+    assert 'name.root' in os.listdir(), \
+        "file not created"
 
 def test_xsec():
     pymcabc.DefineProcess('A A > B B',mA=4,mB=10,mC=1,Ecm=30)
