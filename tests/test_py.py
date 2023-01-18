28bcb3de3a21946668f3dedbeeda9857c98b4b90
amanmdesai <amanmukeshdesai@gmail.com>
2023-01-18 13:18:34 +0530

    add

--- tests/test_py.py
+++ tests/test_py.py
@@ -6,13 +6,11 @@ def test_eventGen():
     pymcabc.DefineProcess('A A > B B',mA=4,mB=10,mC=1,Ecm=30)
     pymcabc.CrossSection().calc_xsection()
     pymcabc.PlotData.file('name.root')
-    assert 'name.root' in os.listdir(), \
-        "file not created"
 
 def test_xsec():
     pymcabc.DefineProcess('A A > B B',mA=4,mB=10,mC=1,Ecm=30)
     sigma, error = pymcabc.CrossSection().calc_xsection()
-    assert sigma <= 10e-14, \
+    assert sigma >= 10e-14, \
         "Sigma under estimated"
-    assert sigma >= 10e-12, \
+    assert sigma <= 10e-12, \
         "Sigma over estimated"
