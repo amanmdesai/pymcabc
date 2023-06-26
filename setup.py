3311f9155a251a64d6e03159a7651356bfb39a71
amanmdesai <amanmukeshdesai@gmail.com>
2023-06-26 10:16:06 +0530

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as file:
 
 setuptools.setup(
 name="pymcabc",
-version="0.3.0",
+version="0.5.0",
 description= "Monte Carlo Event Generator for the ABC theory",
 author="Aman Desai",
 author_email="amanmukeshdesai@gmail.com",
